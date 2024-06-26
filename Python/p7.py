9eaa5ade891c99962e06d86511b8e3bd3268ddb1
Ratnadeep Gurav <deepgurav2329@gmail.com>
2024-06-26 15:09:42 +0530

    adding file

---
+++ Python/p7.py
@@ -0,0 +1,65 @@
+# Giving input from user and give output 
+
+# dict1 ={
+#     "name" :"Ratnadeep",
+#     "School" : "Pune",
+#     "age"   : 20
+# }
+
+# input_options = input(" Enter an Option u want to fetch ")
+
+# print(dict1[input_options])
+
+# #Give 8 numbers from input user and give unique numbers
+# set_1=set()
+# a=int(input("Enter first number"))
+# set_1.add(a)     
+# b=int(input("Enter second number"))
+# set_1.add(b)  
+# c=int(input("Enter third number"))
+# set_1.add(c) 
+# d=int(input("Enter fourth number"))
+# set_1.add(d) 
+# e=int(input("Enter fifth number"))
+# set_1.add(e) 
+# f=int(input("Enter sixth number"))
+# set_1.add(f) 
+# g=int(input("Enter seven number"))
+# set_1.add(g) 
+# h=int(input("Enter eight number"))
+# set_1.add(h) 
+
+# print(set_1)
+
+#Give length of set
+
+# set_2=set()
+
+# set_2.add(20)
+# set_2.add(20.0)
+# set_2.add("20") 
+
+# print(len(set_2)) #output is 2 cause 20 == 20.0 is same
+
+#create an empty dictionary with friends name and value is favourite language
+
+# dict = {}
+
+# name=input("Enter name")
+# lag=input("Enter Language ")
+# dict.update({name:lag})
+
+# name1=input("Enter name")
+# lag1=input("Enter Language ")
+# dict.update({name1:lag1})
+
+# name2=input("Enter name")
+# lag2=input("Enter Language ")
+# dict.update({name2:lag2})
+
+# name3=input("Enter name")
+# lag3=input("Enter Language ")
+# dict.update({name3:lag3})
+
+# print(dict)
+
